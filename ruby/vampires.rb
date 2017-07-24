9ff1d4751fbcf5ed73fceefbe1a6470dffaafebd
Gordon B <gordon.indeed@gmail.com>
2017-07-24 00:20:08 -0700

    Add info gathering questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Would you like some garlic bread?"
+gar_bread = gets.chomp
+
+puts "Would you like to enroll in the company Health Plan?"
+health = gets.chomp
