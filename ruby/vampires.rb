57ff0df859f29ccf4bf4ce2655337defc654eb58
Gordon B <gordon.indeed@gmail.com>
2017-07-24 01:02:57 -0700

    Add control logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,9 @@
+puts "How many employees will be processed?"
+
+
+
 puts "What is your name?"
-name = gets.chomp
+their_name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
@@ -7,8 +11,59 @@ age = gets.chomp
 puts "What year were you born?"
 birth_year = gets.chomp
 
-puts "Would you like some garlic bread?"
+puts "Would you like some garlic bread? (y/n)"
 gar_bread = gets.chomp
 
-puts "Would you like to enroll in the company Health Plan?"
+puts "Would you like to enroll in the company Health Plan? (y/n)"
 health = gets.chomp
+
+result = ""
+
+if age.to_i == (2017 - birth_year.to_i)
+  age = true
+else
+  age = false
+end
+
+if health == "y"
+  health = true
+elsif health == "n"
+  health = false
+else
+  puts ""
+  puts "You didn't give an appropriate answer to the health plan question."
+end
+
+if gar_bread == "y"
+  gar_bread = true
+elsif gar_bread == "n"
+  gar_bread = false
+else
+  puts ""
+  puts "You didn't give an appropriate answer to the garlic bread question."
+end
+
+
+######
+
+case
+when age && gar_bread && health
+  result = "Probably not a vamp"
+when !age && (!gar_bread || !health)
+  result = "Probably a vamp"
+when !age && !gar_bread && !health
+  result = "Almost certainly a vamp"
+when their_name == "Drake Cula" || their_name == "Tu Fang"
+ result = "Definitely a vampire"
+else
+ result = "Something's gone awry"
+end
+
+puts ""
+puts "Report for #{their_name}:"
+puts "Age correct? #{age}"
+puts "Wants garlic bread? #{gar_bread}"
+puts "Wants to enroll? #{health}"
+
+puts "Result of survey is: #{result}"
+puts ""
