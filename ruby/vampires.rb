df275cd64516e5857b8f1bfb630996dd1ba1e868
Gordon B <gordon.indeed@gmail.com>
2017-07-24 01:09:03 -0700

    Add loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,69 +1,77 @@
 puts "How many employees will be processed?"
+applicants = gets.chomp.to_i
+
+i = 0
+
+def survey
+  puts "What is your name?"
+  their_name = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp
+
+  puts "What year were you born?"
+  birth_year = gets.chomp
+
+  puts "Would you like some garlic bread? (y/n)"
+  gar_bread = gets.chomp
+
+  puts "Would you like to enroll in the company Health Plan? (y/n)"
+  health = gets.chomp
+
+  result = ""
+
+  if age.to_i == (2017 - birth_year.to_i)
+    age = true
+  else
+    age = false
+  end
+
+  if health == "y"
+    health = true
+  elsif health == "n"
+    health = false
+  else
+    puts ""
+    puts "You didn't give an appropriate answer to the health plan question."
+  end
+
+  if gar_bread == "y"
+    gar_bread = true
+  elsif gar_bread == "n"
+    gar_bread = false
+  else
+    puts ""
+    puts "You didn't give an appropriate answer to the garlic bread question."
+  end
 
 
+  ######
 
-puts "What is your name?"
-their_name = gets.chomp
+  case
+  when age && gar_bread && health
+    result = "Probably not a vamp"
+  when !age && (!gar_bread || !health)
+    result = "Probably a vamp"
+  when !age && !gar_bread && !health
+    result = "Almost certainly a vamp"
+  when their_name == "Drake Cula" || their_name == "Tu Fang"
+   result = "Definitely a vampire"
+  else
+   result = "Something's gone awry"
+  end
 
-puts "How old are you?"
-age = gets.chomp
-
-puts "What year were you born?"
-birth_year = gets.chomp
-
-puts "Would you like some garlic bread? (y/n)"
-gar_bread = gets.chomp
-
-puts "Would you like to enroll in the company Health Plan? (y/n)"
-health = gets.chomp
-
-result = ""
-
-if age.to_i == (2017 - birth_year.to_i)
-  age = true
-else
-  age = false
-end
-
-if health == "y"
-  health = true
-elsif health == "n"
-  health = false
-else
   puts ""
-  puts "You didn't give an appropriate answer to the health plan question."
-end
+  puts "Report for #{their_name}:"
+  puts "Age correct? #{age}"
+  puts "Wants garlic bread? #{gar_bread}"
+  puts "Wants to enroll? #{health}"
 
-if gar_bread == "y"
-  gar_bread = true
-elsif gar_bread == "n"
-  gar_bread = false
-else
+  puts "Result of survey is: #{result}"
   puts ""
-  puts "You didn't give an appropriate answer to the garlic bread question."
 end
 
-
-######
-
-case
-when age && gar_bread && health
-  result = "Probably not a vamp"
-when !age && (!gar_bread || !health)
-  result = "Probably a vamp"
-when !age && !gar_bread && !health
-  result = "Almost certainly a vamp"
-when their_name == "Drake Cula" || their_name == "Tu Fang"
- result = "Definitely a vampire"
-else
- result = "Something's gone awry"
+while i < applicants
+  survey
+  i += 1
 end
-
-puts ""
-puts "Report for #{their_name}:"
-puts "Age correct? #{age}"
-puts "Wants garlic bread? #{gar_bread}"
-puts "Wants to enroll? #{health}"
-
-puts "Result of survey is: #{result}"
-puts ""
