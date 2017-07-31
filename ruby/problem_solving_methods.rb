74ae285a5e35b7d0f0a1ed2f110826af7bd112c0
Gordon B <gordon.indeed@gmail.com>
2017-07-31 03:40:09 -0700

    Implement insertion sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -10,6 +10,56 @@ def search_array(values, search_term)
   return index
 end
 
-arr = [42, 89, 23, 1]
-p search_array(arr, 89)
+def fib(terms)
+  terms -= 2
+  seq = [0,1]
+  previous = seq[0]
+  current = seq[1]
+  terms.times do |x|
+    current = current + previous
+    seq.push(current)
+    previous = seq[-2]
+  end
+  return seq
+end
 
+
+#pseudocode
+#loop from beginning of array to end
+
+  #starting with [0] and [1],
+  #set key equal to value at index i
+  #set j, the number to the left to be compared to, to i-1
+  #until j is 0 and the value at index j is greater than the key
+    #move over value at j by setting j+1 to be equal to current j
+    #set current j equal to the key
+    #deincrement j
+
+
+def insertion_sort(arr)
+  i = 1
+  key = 0
+  while i < arr.length
+
+    key = arr[i]
+    j = i-1;
+
+    while j >= 0 && arr[j] > key
+
+      arr[j+1] = arr[j]
+      arr[j] = key
+      j = j-1
+
+    end
+    i += 1
+  end
+  return arr
+end
+
+arr = [134, 99, 399, 293, 1, 5 ,99 ,5]
+
+p search_array(arr, 293)
+
+p fib(5)
+
+p insertion_sort(arr)
