b903969066c7d48a41d4db3faf45227e521846a3
Gordon B <gordon.indeed@gmail.com>
2017-08-03 16:03:19 -0700

    Made into mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+=begin
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -12,3 +14,31 @@ end
 
 p Shout.yell_angrily("Farts")
 p Shout.yelling_happily("Woohoo")
+
+=end
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words.upcase + "!!!" + " :)"
+  end
+
+end
+
+class Mom
+  include Shout
+end
+
+class Oldman
+  include Shout
+end
+
+
+mom = Mom.new
+oldman = Oldman.new
+
+p mom.yell_angrily("Do your homework")
+p oldman.yelling_happily("No one around for miles")
