b05b571e6a978629d228eb06504e688a2415e0ea
Gordon B <gordon.indeed@gmail.com>
2017-08-06 16:47:36 -0700

    Add guess method

--- ruby/6-7Game/word_guess.rb
+++ ruby/6-7Game/word_guess.rb
@@ -11,19 +11,41 @@ class WordGuess
     @goal_word = word
     @guesses_left = word.length + 5
     @letters_guessed = []
+    self.update_current_word
   end
 
   def update_current_word
     goal_arr = @goal_word.split("")
     @current_word = []
-    goal_arr.each do |x|
-      if @letters_guessed.include?(x)
-         @current_word << x
+    goal_arr.each do |letter|
+      if @letters_guessed.include?(letter)
+         @current_word << letter
       else
         @current_word << "_"
       end
     end
-    @current_word = @current_word.join!
+    @current_word = @current_word.join
+  end
+
+  def guess(letter)
+    if !letters_guessed.include?(letter)
+      @guesses_left -= 1
+      @letters_guessed << letter
+      if @goal_word.include?(letter)
+       puts "A hit! Good job."
+      else
+        puts "Wrong"
+     end
+    else
+      print "You already guessed that letter"
+    end
+    self.update_current_word
+  end
+
+  def print_status
+    status = "#{@current_word}\n You have #{@guesses_left} guesses left"
+    puts status
+    status
   end
 
 end
--- ruby/6-7Game/word_guess_spec.rb
+++ ruby/6-7Game/word_guess_spec.rb
@@ -16,6 +16,16 @@ describe WordGuess do
   end
 
   it "Prints status" do
-    game = WordGuess.new("hotdog")
-    expect(game.print_status).to eq "______\n You have 11 guesses left"
+    game1 = WordGuess.new("hotdog")
+    expect(game1.print_status).to eq "______\n You have 11 guesses left"
+    game2 = WordGuess.new("slime")
+    expect(game2.print_status).to eq "_____\n You have 10 guesses left"
+  end
+
+  it "Guesses a letter" do
+    game = WordGuess.new("tortoise")
+    game.guess("o")
+    expect(game.print_status).to eq "_o__o___\n You have 12 guesses left"
+  end
+
 end
