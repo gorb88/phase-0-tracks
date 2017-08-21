235a7444375b94cda730caa71c6e8a27aab0c2ff
Gordon B <gordon.indeed@gmail.com>
2017-08-21 00:34:33 -0700

    Added plot visualization

--- databases/8-5project/main.rb
+++ databases/8-5project/main.rb
@@ -2,11 +2,11 @@
 
 Features
 
-User selection
-Inventory
-Plant a garden
-Time passage
-Buy seeds
+User selection *
+Inventory *
+Plant a garden *
+Time passage *
+Buy seeds *
 Sell crops
 plants
   tomatoes
@@ -15,21 +15,6 @@ plants
   tomato seeds
   corn seeds
   pepper seeds
-
-
-Methods
-
-  sleep
-  buy seeds
-  plant seeds
-  update db items
-  sell plants
-
-
-
-
-classes
-  plant
 
 
 userinfo
@@ -79,6 +64,7 @@ create_plot = <<-SQL
     plant VARCHAR(255),
     x INT,
     y INT,
+    days_until_harvest INT,
     owner_id INT,
     FOREIGN KEY (owner_id) REFERENCES farmer(id)
   )
@@ -121,6 +107,7 @@ def make_new_user(db, name)
     db.execute("INSERT INTO inventory(tomatoes, corn, peppers, tomato_seeds, corn_seeds, pepper_seeds, owner_id) VALUES (0, 0, 0, 0, 0, 0, ?)", $user_id)
     puts "Welcome, #{name}!"
 end
+
 
 def sleep(db, user_id)
   puts "How many days would you like to sleep?"
@@ -182,7 +169,6 @@ def shop(db, user_id)
 
     case shop_selection
       when "1"
-        #update inventory
         puts "How many tomato seeds would you like to buy?"
           quantity = gets.chomp.to_i
           cost = quantity * 40
@@ -196,7 +182,6 @@ def shop(db, user_id)
           ok
 
       when "2"
-        #update inventory
         puts "How many corn seeds would you like to buy?"
           quantity = gets.chomp.to_i
           cost = quantity * 30
@@ -210,7 +195,6 @@ def shop(db, user_id)
           ok
 
       when "3"
-        #update inventory
         puts "How many pepper seeds would you like to buy?"
           quantity = gets.chomp.to_i
           cost = quantity * 60
@@ -255,7 +239,105 @@ def print_inventory(db, user_id)
     puts x[1].to_s.ljust(15) + " | " + x[2].to_s.rjust(3)
   end
   db.results_as_hash = true
-  ok
+end
+
+def get_plants(db, user_id)
+  db.results_as_hash = false
+  rows = db.execute("SELECT x, y, plant FROM plot WHERE owner_id = ?", user_id)
+  db.results_as_hash = true
+  rows
+end
+
+def seed_quantity(db, user_id, seed)
+  case seed
+    when "1"
+      item = "tomato_seeds"
+    when "2"
+      item = "corn_seeds"
+    when "3"
+      item = "pepper_seeds"
+  end
+  quantity = get_item_quantity(db, user_id, item)
+  if quantity < 1
+    puts "\n"
+    puts "You don't have enough seeds."
+    puts "\n"
+    false
+  else
+    quantity -= 1
+    set_item_quantity(db, user_id, item, quantity)
+    true
+  end
+
+end
+
+
+def plant_seed(db, user_id)
+
+  while true
+  puts "\n"
+  puts "Please enter a column:"
+  x = gets.chomp.to_i
+  puts "\n"
+  puts "Please enter a row:"
+  y = gets.chomp.to_i
+  puts "\n"
+
+  print_inventory(db, $user_id)
+  puts "\n"
+  puts "What would you like to plant?"
+  puts "1 | Tomato"
+  puts "2 | Corn"
+  puts "3 | Pepper"
+  puts "x | nothing, take me back"
+  plant = gets.chomp
+  case plant
+    when "1"
+      seed_check = seed_quantity(db, user_id, plant)
+      plant = "T"
+      days = 10
+    when "2"
+      seed_check = seed_quantity(db, user_id, plant)
+      plant = "C"
+      days = 7
+    when "3"
+      seed_check = seed_quantity(db, user_id, plant)
+      plant = "P"
+      days = 14
+   when "x"
+     break
+   else
+    puts "\n"
+    puts "That's not an option."
+    puts "\n"
+   end
+   if seed_check
+    spot = db.execute("SELECT * FROM plot WHERE x = ? AND y = ? AND owner_id = ?", x, y, user_id)
+    if spot.empty?
+      db.execute("INSERT INTO plot (plant, x, y, days_until_harvest, owner_id) values (?, ?, ?, ?, ?)", plant, x, y, days, user_id)
+    else
+     db.execute("UPDATE plot SET plant = ?, x = ?, y = ?, days_until_harvest = ? WHERE x = ? AND y = ? AND owner_id = ?", plant, x, y, days, x, y, user_id)
+   end
+   end
+  end
+end
+
+def print_plot(db, user_id)
+  plants = get_plants(db, user_id)
+  plot = Array.new(5){Array.new(10,".")}
+  plants.each do |plant|
+    plot[plant[0]][plant[1]] = plant[2]
+  end
+  print_row = ""
+  #p plot
+  plot.each do |row|
+    row.each do |item|
+      print_row += item
+    end
+    puts print_row
+    print_row = ""
+  end
+
 end
 
 def driver(db)
@@ -266,6 +348,8 @@ def driver(db)
     puts "1 | Shop"
     puts "2 | Sleep"
     puts "3 | Inventory"
+    puts "4 | Plant seed"
+    puts "5 | Plot"
     puts "x | Exit"
     puts "\n"
     input = gets.chomp
@@ -274,11 +358,17 @@ def driver(db)
         shop(db, $user_id)
       when "2"
        sleep(db, $user_id)
-     when "3"
+      when "3"
         print_inventory(db, $user_id)
-     when "x"
+        ok
+      when "4"
+        plant_seed(db, $user_id)
+      when "5"
+        print_plot(db, $user_id)
+        ok
+      when "x"
         break
-    else
+      else
      end
    end
 end
